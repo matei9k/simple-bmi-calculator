014586ad2e6e733c396e7f4b86ab3895fa6026ae
Matei Pralea <matei.pralea@proton.me>
2025-04-01 07:44:16 +0300

    Fix author duplication

---
+++ BMI.cs
@@ -0,0 +1,7 @@
+public class BMI
+{
+    public static float Calculate(float weight, float height)
+    {
+        return weight / (height * height);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Title = "BMI Calculator";
+        while (true)
+        {
+            Console.Write("Enter your weight in kilograms: ");
+            string? weightText = Console.ReadLine();
+            if (weightText == null)
+                continue;
+            float weight = float.Parse(weightText);
+            Console.Write("Enter your height in meters: ");
+            string? heightText = Console.ReadLine();
+            if (heightText == null)
+                continue;
+            float height = float.Parse(heightText);
+            Console.WriteLine($"Your BMI is {BMI.Calculate(weight, height)}");
+        }
+    }
+}
